ba5e5ec650e603308c564c5a2e4d1779878ffa1c
John Torres <apodpm@gmail.com>
2023-04-28 01:16:18 +0300

    try: components

--- src/app/debug/page.tsx
+++ src/app/debug/page.tsx
@@ -1,9 +1,19 @@
 import Placeholder from '@/components/Placeholder';
+import Section from '@/components/debug/Section';
+import Steps from '@/components/debug/Steps';
+import Features1 from '@/components/debug/Features1';
+import Features2 from '@/components/debug/Features2';
+import Subscribe from '@/components/debug/Subscribe';
 
 export default function Page() {
   return (
     <>
+      <Section />
+      <Features2 />
+      <Steps />
+      <Features1 />
       <Placeholder />
+      <Subscribe />
     </>
   )
 }
---
+++ src/components/debug/Section.tsx
@@ -0,0 +1,18 @@
+export default function Example() {
+  return (
+    <div className="px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <p className="text-base font-semibold leading-7 text-indigo-500">
+          Lorem ipsum dolor
+        </p>
+        <h2 className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          Header Section
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-300">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+          fugiat veniam occaecat fugiat aliqua.
+        </p>
+      </div>
+    </div>
+  )
+}
