d712ecfd84f9cbb7765e9396334f0f747c66c91a
John Torres <apodpm@gmail.com>
2023-05-03 17:57:07 +0300

    updated: footer

--- src/components/root/Footer.tsx
+++ src/components/root/Footer.tsx
@@ -229,7 +229,7 @@ export default function Footer() {
           "flex justify-between items-center",
         )}>
           <p className="text-xs leading-5 text-gray-500 dark:text-gray-400">
-            &copy; {years} Your Company, Inc. All rights reserved.
+            &copy; {years} Morbi Dapibus, Inc. All rights reserved.
           </p>
           <ThemeToggle />
         </div>
