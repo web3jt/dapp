345c9f3bba04da1599256c7eae40a8a7af9409cb
John Torres <apodpm@gmail.com>
2023-07-24 23:49:36 +0300

    rename finished: nav to Nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import { Inter } from 'next/font/google';
 import { Providers } from './providers';
 import Tip from '@/components/tips/sample2023';
-import Nav from '@/components/root/NavNew';
+import Nav from '@/components/root/Nav';
 import Footer from '@/components/root/Footer';
 import '@/styles/globals.css';
 
